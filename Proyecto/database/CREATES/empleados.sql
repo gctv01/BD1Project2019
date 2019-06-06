036da9fe377c6281789a0e0baea00536b7f34ff8
ezez8 <ezez8@users.noreply.github.com>
2019-06-06 00:13:16 -0400

    terminando creates

--- Proyecto/database/CREATES/empleados.sql
+++ Proyecto/database/CREATES/empleados.sql
@@ -1,59 +1,113 @@
 CREATE TABLE EMPLEADO(
-expediente NUMERIC(4) NOT NULL,
-di NUMERIC(4) NOT NULL,
-nombre VARCHAR(20) NOT NULL,
-apellido VARCHAR(20) NOT NULL,
-apellido2 VARCHAR(20) NOT NULL,
-fecha_nacimiento date NOT NULL,
-genero VARCHAR(1) NOT NULL CONSTRAINT check_genero check(genero in('M', 'F','T')),
-tipo_sangre VARCHAR(3) NOT NULL CONSTRAINT check_sangre check(tipo_sangre in('O+','A+','B+','AB+','O-','A-','B-','AB-')),
-titulo VARCHAR(1) NOT NULL CONSTRAINT check_titulo check(titulo in('B','Q','M','P','I','G')),
-id_supervisor NUMERIC(4),
-nombre2 VARCHAR(20),
+    expediente NUMERIC(4) NOT NULL,
+    di NUMERIC(4) NOT NULL,
+    nombre VARCHAR(20) NOT NULL,
+    apellido VARCHAR(20) NOT NULL,
+    apellido2 VARCHAR(20) NOT NULL,
+    fecha_nacimiento date NOT NULL,
+    genero VARCHAR(1) NOT NULL CONSTRAINT check_genero check(genero in('M', 'F','T')),
+    tipo_sangre VARCHAR(3) NOT NULL CONSTRAINT check_sangre check(tipo_sangre in('O+','A+','B+','AB+','O-','A-','B-','AB-')),
+    titulo VARCHAR(1) NOT NULL CONSTRAINT check_titulo check(titulo in('B','Q','M','P','I','G')),
+    id_supervisor NUMERIC(4),
+    nombre2 VARCHAR(20),
 
-PRIMARY KEY(expediente),
-FOREIGN KEY(id_supervisor) REFERENCES EMPLEADO(expediente)
+    PRIMARY KEY(expediente),
+    FOREIGN KEY(id_supervisor) REFERENCES EMPLEADO(expediente)
 );
 
 CREATE TABLE COND_SALUD(
-id NUMERIC(4) NOT NULL,
-nombre VARCHAR(20) NOT NULL,
-alergia BOOLEAN NOT NULL,
-descripcion VARCHAR(30),
+    id NUMERIC(4) NOT NULL,
+    nombre VARCHAR(20) NOT NULL,
+    alergia BOOLEAN NOT NULL,
+    descripcion VARCHAR(30),
 
-PRIMARY KEY(id)
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE E_C(
-id_empleado NUMERIC(4) NOT NULL,
-id_cond NUMERIC(4) NOT NULL,
+    id_empleado NUMERIC(4) NOT NULL,
+    id_cond NUMERIC(4) NOT NULL,
 
-PRIMARY KEY(id_empleado,id_cond),
-FOREIGN KEY(id_empleado) REFERENCES EMPLEADO(expediente),
-FOREIGN KEY(id_cond) REFERENCES COND_SALUD(id)
+    PRIMARY KEY(id_empleado,id_cond),
+    FOREIGN KEY(id_empleado) REFERENCES EMPLEADO(expediente),
+    FOREIGN KEY(id_cond) REFERENCES COND_SALUD(id)
 );
 
 /*CREATE TABLE TELEFONO(
-cod NUMERIC(4) NOT NULL,
-num NUMERIC(4) NOT NULL,
-id_empleado NUMERIC(4),
-id_cliente NUMERIC(4),
+    cod NUMERIC(4) NOT NULL,
+    num NUMERIC(4) NOT NULL,
+    id_empleado NUMERIC(4),
+    id_cliente NUMERIC(4),
 
-PRIMARY KEY(cod,num),
-FOREIGN KEY(id_empleado) REFERENCES EMPLEADO(id),
-FOREIGN KEY(id_cliente) REFERENCES CLIENTE(id)
+    PRIMARY KEY(cod,num),
+    FOREIGN KEY(id_empleado) REFERENCES EMPLEADO(id),
+    FOREIGN KEY(id_cliente) REFERENCES CLIENTE(id)
 );*/
 
 CREATE TABLE DETALLE_EXP(
-id NUMERIC(4) NOT NULL,
-fecha DATE NOT NULL,
-motivo VARCHAR(2) NOT NULL CONSTRAINT check_motivo check(motivo in('IN','BM','BA','AM','RE','HE')),
-id_empleado NUMERIC(4) NOT NULL,
-descripcion VARCHAR(30),
-monto_bono NUMERIC(4,2),
-horas_extras
+    id NUMERIC(4) NOT NULL,
+    fecha DATE NOT NULL,
+    motivo VARCHAR(2) NOT NULL CONSTRAINT check_motivo check(motivo in('IN','BM','BA','AM','RE','HE')),
+    id_empleado NUMERIC(4) NOT NULL,
+    descripcion VARCHAR(30),
+    monto_bono NUMERIC(4,2),
+    horas_extras NUMERIC(4),
+    retraso NUMERIC(4),
 
-PRIMARY KEY(id,id_empleado),
-FOREIGN KEY(id_empleado) REFERENCES EMPLEADO(expediente),
+    PRIMARY KEY(id,id_empleado),
+    FOREIGN KEY(id_empleado) REFERENCES EMPLEADO(expediente)
 );
 
+CREATE TABLE ORGANIGRAMA(
+    id NUMERIC(4) NOT NULL,
+    nombre VARCHAR(20) NOT NULL,
+    tipo VARCHAR(1) NOT NULL CONSTRAINT check_tipo check(tipo in('G','S','D','A')),
+    nivel NUMERIC(1) NOT NULL CONSTRAINT check_nivel check(nivel in(1,2,3,4)),
+    id_superior NUMERIC(4),
+    descripcion VARCHAR(30),
+
+    PRIMARY KEY(id),
+    FOREIGN KEY(id_superior) REFERENCES ORGANIGRAMA(id)
+);
+
+CREATE TABLE EMPLEO(
+    fecha_inicio DATE NOT NULL,
+    sueldo NUMERIC(4,2) NOT NULL,
+    cargo VARCHAR(1) NOT NULL CONSTRAINT check_cargo check(cargo in('S','G','O','E','M','I')),
+    id_empleado NUMERIC(4) NOT NULL,
+    id_organigrama NUMERIC(4) NOT NULL,
+    fecha_fin DATE,
+
+    PRIMARY KEY(fecha_inicio,id_empleado),
+    FOREIGN KEY(id_empleado) REFERENCES EMPLEADO(expediente),
+    FOREIGN KEY(id_organigrama) REFERENCES ORGANIGRAMA(id)
+);
+
+CREATE TABLE TURNOS_HIST(
+    fecha_inicio DATE NOT NULL,
+    turno NUMERIC(1) NOT NULL CONSTRAINT check_turno check(turno in(1,2,3)),
+    id_empleo DATE NOT NULL,
+    fecha_fin DATE,
+
+    PRIMARY KEY(fecha_inicio,id_empleo),
+    FOREIGN KEY(id_empleo) REFERENCES EMPLEO(fecha_inicio)
+);
+
+CREATE TABLE REUNION(
+    id NUMERIC(4) NOT NULL,
+    fecha DATE NOT NULL,
+    minuta VARCHAR(30) NOT NULL,
+    id_supervisor NUMERIC(4) NOT NULL,
+
+    PRIMARY KEY(id,id_supervisor),
+    FOREIGN KEY(id_supervisor) REFERENCES EMPLEADO(expediente)
+);
+
+CREATE TABLE INASISTENCIA(
+    id_empleo DATE NOT NULL,
+    id_reunion NUMERIC(4) NOT NULL,
+
+    PRIMARY KEY(id_empleo,id_reunion),
+    FOREIGN KEY(id_empleo) REFERENCES EMPLEO(fecha_inicio),
+    FOREIGN KEY(id_reunion) REFERENCES REUNION(id)
+);
