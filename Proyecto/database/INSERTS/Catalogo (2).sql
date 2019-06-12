0aef362d1e8e6b29ff1289378cc8a17b5f68ae08
javier0997 <39354554+javier0997@users.noreply.github.com>
2019-06-12 02:05:25 -0400

    Add files via upload

---
+++ Proyecto/database/INSERTS/Catalogo (2).sql
@@ -0,0 +1,164 @@
+
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (1, 'Bandeja', , 'Rectangular', , , '44x22cm', );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (2, 'Plato', 'Llano', 'Cuadrado', , , '27x27cm', );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (3, 'Tetera', , , , 6, , );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (4, 'Taza', , , 'MpkaSp', , , );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (5, 'Azucarero', , , , 6, , );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (6, 'Taza', , , 'CafeSp', , , );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (7, 'Bandeja', , 'Ovalado', , , '44x22cm', );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (8, 'Tetera', , , , 2, , );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (9, 'Taza', , , 'TeCp', , , );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (10, 'Ensaladera', , , , , '30x18cm', );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (11, 'Azucarero', , , , 6, , );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (12, 'Lechera', , , , 6, , );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (13, 'Plato', 'Llano', 'Rectangular', , , '27x27cm', );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (14, 'Taza', , , 'MpkaSp', , , );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (15, 'Bandeja', , 'Ovalado', , , '35x18cm', );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (16, 'Plato', 'Presenta', 'Redondo', , , '14x1cm', );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (17, 'Plato', 'Hondo', 'Cuadrado', , , '2x3cm', );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (18, 'Taza', , , 'TeCp', , , );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (19, 'Bandeja', , 'Ovalado', , , '20x20cm', );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (20, 'Jarra', , , , , , '1.5lt');
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (21, 'Cazuela', , , , , '19x12cm', );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (22, 'Plato', 'Llano', 'Redondo', , , '24x12cm', );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (23, 'Ensaladera', , , , , '22x22cm', );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (24, 'Taza', , , 'MpkaSp', , , );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (25, 'Cazuela', , , , , '7x6cm', );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (26, 'Plato', 'Postre', 'Redondo', , , '24x24cm', );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (27, 'Taza', , , 'CafeCp', , , );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (28, 'Jarra', , , , , , '31lt');
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (29, 'Cazuela', , , , , '15x15cm', );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (30, 'Ensaladera', , , , , '38x38cm', );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (31, 'Jarra', , , , , , '31lt');
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (32, 'Plato', 'Pasta', 'Cuadrado', , , '20x20cm', );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (33, 'Plato', 'Postre', , , , '23x33cm', );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (34, 'Taza', , , 'MokaCp', , , );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (35, 'Azucarero', , , , 2, , );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (36, 'Bandeja', , 'Rectangular', , , '20x20cm', );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (37, 'Plato', 'Postre', , , , '13x12cm', );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (38, 'Taza', , , 'TeSp', , , );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (39, 'Plato', 'Hondo', 'Rectangular', , , '12x12cm', );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (40, 'Plato', 'Llaano', 'Cuadrado', , , '14x14cm', );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (41, 'Cazuela', , , , , '19x19cm', );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (42, 'Cazuela', , , , , '20x20cm', );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (43, 'Ensaladera', , , , , '30x30cm', );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (44, 'Cazuela', , , , , '15x15cm', );
+insert into "Molde" ("id", "tipo", "tipo_plato", "forma", "tipo_taza", "cant_pers", "tamano", "volumen") values (45, 'Lechera', , , , 6, , );
+
+Insert into "Coleccion" ("id", "categoria", "linea", "desc") values (1, 'Clas_ele', 'I', 'Coleccion Amanecer Institucional')
+Insert into "Coleccion" ("id", "categoria", "linea", "desc") values (2, 'Country', 'F', 'Coleccion Tarde Familiar')
+Insert into "Coleccion" ("id", "categoria", "linea", "desc") values (3, 'Moderna', 'F', 'Coleccion Noche Familiar')
+Insert into "Coleccion" ("id", "categoria", "linea", "desc") values (4, 'Country', 'I', 'Coleccion Zen Institucional')
+Insert into "Coleccion" ("id", "categoria", "linea", "desc") values (5, 'Moderna', 'I', 'Coleccion Tiempo Institucional')
+
+Insert into "Col_Mot" ("id", "nombre", "tipo", "desc") values (1, 'Azul', 'Color','Color Azul' )
+Insert into "Col_Mot" ("id", "nombre", "tipo", "desc") values (2, 'Rojo', 'Color','Color Rojo' )
+Insert into "Col_Mot" ("id", "nombre", "tipo", "desc") values (3, 'Animales', 'Motivo','Motivo Animales' )
+Insert into "Col_Mot" ("id", "nombre", "tipo", "desc") values (4, 'Arboles', 'Motivo','Motivo Arboles' )
+Insert into "Col_Mot" ("id", "nombre", "tipo", "desc") values (5, 'Amarillo', 'Color','Color Amarillo' )
+
+insert into "Juego" ("id", "cant_personas", "nombre", "desc") values (1, 2, 'Marron', 'Juego Marron 2 Personas');
+insert into "Juego" ("id", "cant_personas", "nombre", "desc") values (2, 4, 'Mar', 'Juego Mar 4 personas');
+insert into "Juego" ("id", "cant_personas", "nombre", "desc") values (3, 6, 'Llano', 'Juego Llano 6 personas');
+insert into "Juego" ("id", "cant_personas", "nombre", "desc") values (4, 2, 'Rudo', 'Juego Rudo 2 personas');
+insert into "Juego" ("id", "cant_personas", "nombre", "desc") values (5, 6, 'Fiesta', 'Juego Fiesta 6 personas');
+insert into "Juego" ("id", "cant_personas", "nombre", "desc") values (6, 6, 'Rumba', 'Juego Rumba 6 personas');
+insert into "Juego" ("id", "cant_personas", "nombre", "desc") values (7, 2, 'Dorado', 'Juego Dorado 2 personas');
+insert into "Juego" ("id", "cant_personas", "nombre", "desc") values (8, 6, 'Postre', 'Juego Postre 6 personas');
+insert into "Juego" ("id", "cant_personas", "nombre", "desc") values (9, 4, 'Sala', 'Juego Sala 4 personas');
+insert into "Juego" ("id", "cant_personas", "nombre", "desc") values (10, 6, 'Cocina', 'Juego Cocina 6 personas');
+
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (1, 1, 1, 1, 'Bandeja Rectangular 44x22cm Coleccion 1' );
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (2, 1, 1, 2, 'plato llano cuadrado 27x27cm Coleccion 1' );
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (3, 1, 1, 3, 'tetera 6pers Coleccion 1 ');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (4, 1, 1, 4, 'taza moka sin plato Coleccion 1 ');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (5, 1, 1, 5, 'azucarero 6pers Coleccion 1 ');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (6, 1, 1, 6, 'tazas cafe sin plato Coleccion 1 ');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (7, 1, 1, 7, '1 bandeja ovalada 44x22cm Coleccion 1 ');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (8, 1, 1, 8, '1 tetera 2pers Coleccion 1 ');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (9, 1, 1, 9, 'taza te con plato Coleccion 1 ');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (10, 1, 1, 10, '1 ensaladera 30x18cm Coleccion 1 ');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (11, 1, 1, 11, '1 azucarero 6pers Coleccion 1 ');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (12, 2, 2, 12, 'lechera 2pers');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (13, 2, 2, 13, 'plato llano rectangular 27x27cm');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (14, 2, 2, 14, 'taza moka sin plato');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (15, 2, 2, 15, 'bandeja ovalada 35x18cm');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (16, 2, 2, 16, 'plato presentacion redondo 14x1cm');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (17, 2, 2, 17, 'platos hondo cuadrado 2x3cm');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (18, 2, 2, 18, 'taza te con plato');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (19, 2, 2, 3, 'tetera 6pers');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (20, 2, 2, 19, 'bandeja ovalada 20x20cm');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (21, 2, 2, 20, 'jarra 1.5lts');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (22, 2, 2, 21, 'cazuela 19x12cm');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (23, 2, 2, 22, 'plastos llanos redondo 24x12cm');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (24, 3, 3, 23, 'ensaladera 22x22cm');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (26, 3, 3, 25, 'cazuela 7x6cm');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (27, 3, 3, 26, 'plato postre redondo 24x24cm');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (28, 3, 3, 27, 'taza cafe con plato');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (29, 3, 3, 28, 'jarra 3lts');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (30, 3, 3, 6, 'taza cafe si plato');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (31, 3, 3, 29, 'cazuela 15x15cm');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (31, 3, 3, 30, 'ensaladera 38x38cm');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (33, 4, 4, 9, 'taza te con plato');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (34, 4, 4, 31, 'jarra 1lts');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (35, 4, 4, 32, 'plato pasta cuadrado 12x12cm');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (36, 4, 4, 36, 'bandeja rectangular 20x20cm');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (37, 4, 4, 6, 'taza cafe sin plato');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (38, 4, 4, 33, 'plato postre 23x33cm');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (39, 4, 4, 5, 'azucarero 6pers');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (40, 4, 4, 34, 'tazas moka con plato');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (41, 4, 4, 8, 'tetera 2pers');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (42, 4, 4, 37, 'plato postre 13x12cm');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (43, 4, 4, 27, ' taza cafe con plato');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (44, 4, 4, 35, ' azucarera 2pers');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (45, 5, 5, 38, 'taza te sin plato');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (46, 5, 5, 39, 'plato hondo rectangular 12x12cm');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (47, 5, 5, 40, 'plato llano cuadrado 14x14cm');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (48, 5, 5, 45, 'lechera 6pers');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (49, 5, 5, 41, 'cazuela 19x19cm');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (50, 5, 5, 42, 'cazuela 20x20cm');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (51, 5, 5, 34, 'taza moka con plato');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (52, 5, 5, 43, 'ensaladeras 30x30cm');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (53, 5, 5, 44, 'cazuelas 15x15cm');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (54, 5, 5, 28, 'jarra 3lts');
+insert into "Pieza" ("id", "id_Coleccion", "id_Col_Mot", "id_Molde", "desc") values (55, 5, 5, 6, 'taza cafe sin plato');
+
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (1, 6, 1, 0);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (2, 7, 1, 1);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (3, 8, 1, 1);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (4, 9, 2, 0);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (5, 10, 2, 1);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (6, 11, 2, 1);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (7, 17, 3, 0);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (8, 18, 3, 0);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (9, 19, 3, 2);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (10, 20, 4, 1);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (11, 21, 4, 1);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (12, 22, 4, 2);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (13, 23, 2, 0);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (14, 28, 5, 1);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (15, 29, 5, 0);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (16, 30, 6, 2);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (17, 31, 6, 2);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (18, 32, 6, 2);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (19, 38, 7, 0);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (20, 39, 7, 1);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (21, 40, 7, 0);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (22, 41, 7, 1);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (23, 42, 8, 0);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (24, 43, 8, 0);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (25, 44, 8, 1);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (26, 30, 9, 3);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (27, 31, 9, 0);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (28, 32, 10, 4);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (29, 28, 10, 2);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (30, 29, 10, 2);
+insert into "J_P" ("id", "id_Pieza", "id_Juego", "cantidad") values (31, 30, 10, 0);
+
+Insert into "C_M" ("id", "id_Col_Mot", "id_Coleccion") values (1,1,1)
+Insert into "C_M" ("id", "id_Col_Mot", "id_Coleccion") values (2,2,2)
+Insert into "C_M" ("id", "id_Col_Mot", "id_Coleccion") values (3,3,3)
+Insert into "C_M" ("id", "id_Col_Mot", "id_Coleccion") values (4,4,4)
+Insert into "C_M" ("id", "id_Col_Mot", "id_Coleccion") values (5,5,5)
+
+Insert into "Hist_Pieza" ("fecha", "precio_Bs", "id_Pieza") values ('01-01-2019', 15000, 10, 1)
