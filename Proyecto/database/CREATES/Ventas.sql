f7ef63160720d878fd97d0a11dff4ac78ed05300
Giancarlo <giankitricerri@gmail.com>
2019-06-21 20:58:43 -0400

    create ventas

--- Proyecto/database/CREATES/Ventas.sql
+++ Proyecto/database/CREATES/Ventas.sql
@@ -1,73 +1,57 @@
-CREATE TABLE public."Cliente"
+﻿CREATE TABLE EJGCliente
 (
-    id numeric NOT NULL,
-    “nombre“ character varying COLLATE pg_catalog."default" NOT NULL,
-    “pais” character varying COLLATE pg_catalog."default",
-    CONSTRAINT "Cliente_pkey" PRIMARY KEY (id),
-    CONSTRAINT "Check_Pais" CHECK (pais::text = ANY (ARRAY['VEN'::character varying, 'USA'::character varying, 'COL'::character varying, 'REPDOM'::character varying]::text[])) NOT VALID
+    id SERIAL NOT NULL,
+    nombre character varying NOT NULL,
+    pais character varying,
+    CONSTRAINT Cliente_pkey PRIMARY KEY (id),
+    CONSTRAINT Check_Pais CHECK (pais::text = ANY (ARRAY['VEN'::character varying, 'USA'::character varying, 'COL'::character varying, 'REPDOM'::character varying]::text[])) NOT VALID
 )
-WITH (
-    OIDS = FALSE
-)
-TABLESPACE pg_default;
 
-CREATE TABLE public."Contrato"
+CREATE TABLE EJGContrato
 (
-    "numero" numeric NOT NULL,
-    "descuento" character varying COLLATE pg_catalog."default" NOT NULL,
-    "id_Cliente" numeric NOT NULL,
-    CONSTRAINT "Contrato_pkey" PRIMARY KEY (numero),
-	CONSTRAINT "FK_Cliente" FOREIGN KEY ("id_Cliente")
-        REFERENCES public."Cliente" (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
+    numero SERIAL NOT NULL,
+    descuento character varying NOT NULL,
+    id_Cliente numeric NOT NULL,
+    CONSTRAINT Contrato_pkey PRIMARY KEY (numero),
+	CONSTRAINT FK_Cliente FOREIGN KEY (id_Cliente)
+        REFERENCES Cliente (id) 
+        
    
 )
-WITH (
-    OIDS = FALSE
-)
-TABLESPACE pg_default;
 
-CREATE TABLE public."Pedido"
+
+CREATE TABLE EJGPedido
 (
-    "numero" numeric NOT NULL,
-    "fecha_encargo" date NOT NULL,
-    "fecha_entrega" date NOT NULL,
-	"fecha_emision" date NOT NULL,
-    CONSTRAINT "Pedido_pkey" PRIMARY KEY (numero)
+    numero SERIAL NOT NULL,
+    fecha_encargo date NOT NULL,
+    echa_entrega date NOT NULL,
+   fecha_emision date NOT NULL,
+    CONSTRAINT Pedido_pkey PRIMARY KEY (numero)
 	
    
 )
-WITH (
-    OIDS = FALSE
-)
-TABLESPACE pg_default;
 
-CREATE TABLE public."Factura"
+CREATE TABLE EJGFactura
 (
     
-    id numeric NOT NULL,
-    "Fk_Pedido" numeric NOT NULL,
-	"monto_total" numeric NOT NULL,
-    "fechaemision" date NOT NULL,
-    CONSTRAINT "Factura_pkey" PRIMARY KEY (id),
-    CONSTRAINT "FK_Pedido" FOREIGN KEY ("Fk_Pedido")
-        REFERENCES public."Pedido" ("numero") MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-)
+    id SERIAL NOT NULL,
+    Fk_Pedido numeric NOT NULL,
+	monto_total numeric NOT NULL,
+    fechaemision date NOT NULL,
+    CONSTRAINT Factura_pkey PRIMARY KEY (id),
+    CONSTRAINT FK_Pedido FOREIGN KEY (Fk_Pedido)
+        REFERENCES Pedido (numero) 
 
-CREATE TABLE public."Detalle"
+CREATE TABLE Detalle
 (
     
-    "cantidad" numeric NOT NULL,
-    "id_Pieza" numeric NOT NULL,
-	"id_Juego" numeric NOT NULL,
-    CONSTRAINT "Detalle_pkey" PRIMARY KEY ("id_Pieza", "id_Juego"),
-    CONSTRAINT "FK_Juego" FOREIGN KEY ("id_Juego")
-        REFERENCES public."Juego" (id) MATCH SIMPLE
-        ON UPDATE NO ACTION,
-	CONSTRAINT "FK_Pieza" FOREIGN KEY ("id_Juego")
-        REFERENCES public."Pieza" (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
+    cantidad SERIAL NOT NULL,
+    id_Pieza numeric NOT NULL,
+	id_Juego integer NOT NULL,
+    CONSTRAINT Detalle_pkey PRIMARY KEY (id_Pieza, id_Juego),
+    CONSTRAINT FK_Juego FOREIGN KEY (id_Juego)
+        REFERENCES Juego (id) 
+	CONSTRAINT FK_Pieza FOREIGN KEY (id_Juego)
+        REFERENCES Pieza (id)
+        
 )
