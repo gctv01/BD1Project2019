c7d6a6d945e623d79b3b5368cdc42d4f6101b1ab
Giancarlo <giankitricerri@gmail.com>
2019-07-03 08:59:47 -0400

    Lista mi reflexion

--- Proyecto/database/REPORTES/empleados.sql
+++ Proyecto/database/REPORTES/empleados.sql
@@ -1,3 +1,3 @@
 select di,nombre,apellido,apellido2,fecha_nacimiento,genero,tipo_sangre,titulo,nombre2,
-(select emps.di from empleado as emps where emp.fk_supervisor = emps.expediente) di_supervisor 
-from empleado as emp;
+(select emps.di from ejgempleado as emps where emp.fk_supervisor = emps.expediente) di_supervisor 
+from ejgempleado as emp;
